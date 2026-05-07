e179a5c038aa65e4465581de2696463bee1d4cf8
Mateus Moreira <moreiramateusjp@gmail.com>
2026-05-07 13:06:27 -0300

    criando estrutura do projeto templates

--- core/views.py
+++ core/views.py
@@ -1,13 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
-
-#teste
-
-def horario(request):
-    from datetime import datetime
-    
-    return render(request, 'partials/horario.html', {
-        'hora': datetime.now()
-    })
+    return render(request, 'home/index.html')
--- selettoTernos/urls.py
+++ selettoTernos/urls.py
@@ -16,13 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import index, horario
+from core.views import index
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    #teste 
-    path('horario/', horario),
 ]
